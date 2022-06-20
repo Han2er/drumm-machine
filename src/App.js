bbfaa95dae8f824c895bb6170c9ccfbcc0fb3707
Han2er <han2er@live.com>
2022-06-21 01:42:23 +0400

    feat: add bank two

--- src/App.js
+++ src/App.js
@@ -58,70 +58,72 @@ const bankOne = [
   },
 ];
 
-// const bankTwo = [
-//   {
-//     keyCode: 81,
-//     keyTrigger: "Q",
-//     id: "Chord-1",
-//     url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
-//   },
-//   {
-//     keyCode: 87,
-//     keyTrigger: "W",
-//     id: "Chord-2",
-//     url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"
-//   },
-//   {
-//     keyCode: 69,
-//     keyTrigger: "E",
-//     id: "Chord-3",
-//     url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"
-//   },
-//   {
-//     keyCode: 65,
-//     keyTrigger: "A",
-//     id: "Shaker",
-//     url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
-//   },
-//   {
-//     keyCode: 83,
-//     keyTrigger: "S",
-//     id: "Open-HH",
-//     url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"
-//   },
-//   {
-//     keyCode: 68,
-//     keyTrigger: "D",
-//     id: "Closed-HH",
-//     url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"
-//   },
-//   {
-//     keyCode: 90,
-//     keyTrigger: "Z",
-//     id: "Punchy-Kick",
-//     url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
-//   },
-//   {
-//     keyCode: 88,
-//     keyTrigger: "X",
-//     id: "Side-Stick",
-//     url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
-//   },
-//   {
-//     keyCode: 67,
-//     keyTrigger: "C",
-//     id: "Snare",
-//     url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
-//   }
-// ];
+const bankTwo = [
+  {
+    keyCode: 81,
+    keyTrigger: "Q",
+    id: "Chord-1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+  },
+  {
+    keyCode: 87,
+    keyTrigger: "W",
+    id: "Chord-2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+  },
+  {
+    keyCode: 69,
+    keyTrigger: "E",
+    id: "Chord-3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+  },
+  {
+    keyCode: 65,
+    keyTrigger: "A",
+    id: "Shaker",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+  },
+  {
+    keyCode: 83,
+    keyTrigger: "S",
+    id: "Open-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+  },
+  {
+    keyCode: 68,
+    keyTrigger: "D",
+    id: "Closed-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+  },
+  {
+    keyCode: 90,
+    keyTrigger: "Z",
+    id: "Punchy-Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+  },
+  {
+    keyCode: 88,
+    keyTrigger: "X",
+    id: "Side-Stick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+  },
+  {
+    keyCode: 67,
+    keyTrigger: "C",
+    id: "Snare",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+  },
+];
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      key: 1,
+      bank: bankOne,
     };
     this.tap = this.tap.bind(this);
+    this.switchToBank1 = this.switchToBank1.bind(this);
+    this.switchToBank2 = this.switchToBank2.bind(this);
   }
   tap(e) {
     const id = e.target.innerText;
@@ -129,20 +131,36 @@ class App extends React.Component {
     sound.currentTime = 0;
     sound.play();
   }
-  drumKeys = bankOne.map((el) => (
-    <div key={el.id} className="drum-pad">
-      {el.keyTrigger}
-      <audio id={el.keyTrigger} src={el.url}></audio>
-    </div>
-  ));
+  switchToBank1(e) {
+    this.setState({ bank: bankOne });
+    console.log(e.target);
+  }
+  switchToBank2(e) {
+    this.setState({ bank: bankTwo });
+  }
+
   render() {
     return (
       <div id="drum-machine">
         <div id="display">
           <div id="key-container" onClick={this.tap}>
-            {this.drumKeys}
+            {this.state.bank.map((el) => (
+              <div key={el.id} className="drum-pad">
+                {el.keyTrigger}
+                <audio id={el.keyTrigger} src={el.url}></audio>
+              </div>
+            ))}
           </div>
-          <div id="control-container"></div>
+          <div id="control-container">
+            <div className="switch">
+              <div className="btn" onClick={this.switchToBank1}>
+                1
+              </div>
+              <div className="btn" onClick={this.switchToBank2}>
+                2
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
